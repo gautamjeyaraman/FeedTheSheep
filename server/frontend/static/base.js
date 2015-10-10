44777d7ace7e5003b4cac9b8f76d9f8f86acfe55
Gautam Jeyaraman <gautam@guest-v44-00421.dhcp.unc.edu>
2015-10-10 13:49:42 -0400

    Added obstacles

--- server/frontend/static/base.js
+++ server/frontend/static/base.js
@@ -44,3 +44,46 @@ function setup() {
     }
   };
 }
+
+
+function drawObstacles(){
+  for(var i=0; i<obstacles.length; i++){
+    if(_.has(obstacles[i], "circle")){
+      drawCircle(obstacles[i]);
+    }
+    if(_.has(obstacles[i], "box")){
+      drawBox(obstacles[i]);
+    }
+  }
+}
+
+function drawCircle(obstacle){
+  obstacle = obstacle.circle;
+  var img = new Image;
+  img.onload = function(){
+    ctx.fillStyle = ctx.createPattern(img, "repeat");
+    ctx.beginPath();
+    ctx.moveTo(obstacle.center.x + radius, obstacle.center.y);
+    ctx.arc(obstacle.center.x, obstacle.center.y, obstacle.radius, 0, 2*Math.PI);
+    ctx.fill();
+  }
+  img.src = "/static/images/obs.png";
+}
+
+function drawBox(obstacle){
+  obstacle = obstacle.box;
+  var img = new Image;
+  img.onload = function(){
+    ctx.fillStyle = ctx.createPattern(img, "repeat");
+    ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.length);
+  }
+  img.src = "/static/images/obs.png";
+}
+
+var obstacles = [
+  {"circle": {"radius": 10, "center": {"x": 100, "y": 200}}},
+  {"circle": {"radius": 50, "center": {"x": 600, "y": 350}}},
+  {"box": {"x": 800, "y": 30, "length": 100, "width": 50}}
+]
+
+drawObstacles();
