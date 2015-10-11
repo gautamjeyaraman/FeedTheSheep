be7df8ae06aed1ab23b35805b1448bee5a22a73d
Gautam Jeyaraman <gautam@dhcp-v80-001420.wireless.unc.edu>
2015-10-11 05:25:48 -0400

    Changed the radius

--- server/frontend/static/base.js
+++ server/frontend/static/base.js
@@ -15,7 +15,7 @@ var initial_percentage = 100;
 var ctx = canvas.getContext("2d"),
     foreground = new Image,
     background = new Image,
-    radius = 50;
+    radius = 40;
     var counts = 0;
 
 var area_without_obstacles = 0;
